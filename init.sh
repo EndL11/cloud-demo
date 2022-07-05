0038019ffb5552fdb75a8434ac20b1405bcf1cd0
EndL11 <podobailo.andriy@gmail.com>
2022-07-05 18:50:48 +0300

    fixes repo name

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ echo 'started init'
 sudo apt-get update
 sudo apt install -y docker.io
 sudo apt install -y docker-compose
-sudo git clone https://github.com/EndL11/cloud-tech-demo2.git && cd cloud-tech-demo
+sudo git clone https://github.com/EndL11/cloud-demo.git && cd cloud-demo
 
 sudo docker-compose up -d
 
