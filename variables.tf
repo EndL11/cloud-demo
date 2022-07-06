18e41663b583b09e6dcbb2ed9ee4628fce528e47
EndL11 <podobailo.andriy@gmail.com>
2022-07-06 19:17:17 +0300

    s

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "secret_key" {
+  description = "AWS SECRET KEY"
+  type        = string
+  sensitive   = true
+}
+variable "access_key" {
+  description = "AWS ACCESS KEY"
+  type        = string
+  sensitive   = true
+}
+
